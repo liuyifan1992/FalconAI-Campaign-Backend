c4b8c9dea5b1f613e78ecfdbd4f21acf0a3e19ef
Yifan Liu <yl2523@cornell.edu>
2024-02-17 17:20:01 -0800

    Change some data model fields to be nullable
    Change some data model fields to be nullable

--- FalconAI_Server/email_generator/models.py
+++ FalconAI_Server/email_generator/models.py
@@ -1,19 +1,21 @@
-from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db import models
 from django.utils import timezone
+
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
+
     def create_user(self, email, first_name, last_name, password=None):
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
 
         if not first_name:
-            raise ValueError('Users must have a first name')
+            raise ValueError("Users must have a first name")
 
         if not last_name:
-            raise ValueError('Users must have a last name')
-        
+            raise ValueError("Users must have a last name")
+
         falcon_ai_user = self.model(
             email=self.normalize_email(email),
             first_name=first_name,
@@ -22,9 +24,11 @@ class UserManager(BaseUserManager):
         falcon_ai_user.set_password(password)
         falcon_ai_user.save()
         return falcon_ai_user
-    
+
     def create_superuser(self, email, first_name, last_name, password=None):
-        falcon_ai_user = self.create_user(email, first_name, last_name, password=password)
+        falcon_ai_user = self.create_user(
+            email, first_name, last_name, password=password
+        )
         falcon_ai_user.is_admin = True
         falcon_ai_user.set_password(password)
         falcon_ai_user.save()
@@ -33,7 +37,7 @@ class UserManager(BaseUserManager):
 
 class FlaconAIUser(AbstractBaseUser):
     username = None
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100, null=False)
     is_active = models.BooleanField(default=True)
@@ -41,18 +45,18 @@ class FlaconAIUser(AbstractBaseUser):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
         return self.email
-    
+
     def has_perm(self, perm, obj=None):
         return True
 
     def has_module_perms(self, app_label):
         return True
-    
+
     @property
     def is_staff(self):
         return self.is_admin
@@ -65,7 +69,7 @@ class Business(models.Model):
 
 class Employee(models.Model):
     employee_id = models.CharField(max_length=100, null=False, primary_key=True)
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100, null=False)
     business = models.ForeignKey(Business, on_delete=models.CASCADE, null=False)
@@ -85,7 +89,6 @@ class Campaign(models.Model):
 
 
 class AIEmail(models.Model):
-
     class WritingStyle(models.IntegerChoices):
         FORMAL = 0
         APPRECIATING = 1
@@ -93,17 +96,17 @@ class AIEmail(models.Model):
         NEUTRAL = 3
 
     email_id = models.CharField(max_length=100, null=False, primary_key=True)
-    campaign = models.ForeignKey(Campaign, null=False, on_delete=models.CASCADE)
-    email_topic = models.CharField(max_length=500, null=False)
-    sender = models.ForeignKey(FlaconAIUser, null=False, on_delete=models.PROTECT)
-    recipient = models.ForeignKey(Employee, null=False, on_delete=models.PROTECT)
+    campaign = models.ForeignKey(Campaign, null=True, on_delete=models.CASCADE)
+    email_topic = models.CharField(max_length=500, null=True)
+    sender = models.ForeignKey(FlaconAIUser, null=True, on_delete=models.PROTECT)
+    recipient = models.ForeignKey(Employee, null=True, on_delete=models.PROTECT)
     writing_style = models.IntegerField(
-        verbose_name='writing style',
+        verbose_name="writing style",
         choices=WritingStyle.choices,
-        default=WritingStyle.FORMAL
+        default=WritingStyle.FORMAL,
+        null=True,
     )
     email_content = models.TextField(null=False)
 
     def __str__(self):
         return self.email_id
-    
