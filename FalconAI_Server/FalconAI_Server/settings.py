845cf4ab0a159e8a48dc5192d8cabb0fa28c3906
ryantobin77 <ryantobin77@gmail.com>
2024-02-18 17:39:01 -0800

    Setup postgres SQL

--- FalconAI_Server/FalconAI_Server/settings.py
+++ FalconAI_Server/FalconAI_Server/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +78,21 @@ WSGI_APPLICATION = 'FalconAI_Server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'falcon_ai_db',
+        'USER': 'postgres',
+        'PASSWORD': 'falconaidb123',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
--- README.md
+++ README.md
@@ -5,8 +5,20 @@ Front-end:
 2. In the project directory, create a folder called "models" and download the model.
 3. run project: streamlit run app.py
 
+Setup Database:
+1. Install Postgres SQL 16 from https://www.postgresql.org/download/macosx/
+2. When installing postgres, make the postgres user password falconaidb123
+3. Create the database
+    1. Open up pgAdmin 4. Click server, and choose PostgreSQL version 16
+    2. Right click on databases and create a new database with the name falcon_ai_db
+4. python manage.py makemigrations
+5. python manage.py migrate
+6. python manage.py createsuperuser
+    1. Email: test@falconai.com
+    2. First Name: Super
+    3. Last Name: User
+    4. Password: falconai123
+
 Back-end:
-1. python3 manage.py makemigrations
-2. python3 manage.py migrate
-3. python3 manage.py runserver
+1. python3 manage.py runserver
 
